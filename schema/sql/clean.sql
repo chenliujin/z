71608236fe985085740408916116d9cab6dd0ac9
chenliujin <liujin.chen@qq.com>
2016-08-27 22:14:03 +0800

    update

---
+++ schema/sql/clean.sql
@@ -0,0 +1,5 @@
+delete from configuration where configuration_key = 'SHOW_BANNERS_GROUP_SET7';
+delete from layout_boxes where layout_box_name = 'banner_box.php';
+
+delete from configuration where configuration_key = 'SHOW_BANNERS_GROUP_SET8';
+delete from layout_boxes where layout_box_name = 'banner_box2.php';   
