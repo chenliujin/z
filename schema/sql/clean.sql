02e887d4f22dda8fb5956077b590bb6dc48e7a14
chenliujin <liujin.chen@qq.com>
2016-09-05 16:09:38 +0800

    remove

--- schema/sql/clean.sql
+++
@@ -1,8 +0,0 @@
-delete from configuration where configuration_key = 'SHOW_BANNERS_GROUP_SET7';
-delete from layout_boxes where layout_box_name = 'banner_box.php';
-
-delete from configuration where configuration_key = 'SHOW_BANNERS_GROUP_SET8';
-delete from layout_boxes where layout_box_name = 'banner_box2.php';
-
-delete from configuration where configuration_key = 'SHOW_BANNERS_GROUP_SET_ALL';
-delete from layout_boxes where layout_box_name = 'banner_box_all.php';
