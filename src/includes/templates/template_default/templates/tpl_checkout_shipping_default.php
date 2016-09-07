5290a54f7fe95a023a0fc317b4b2628b1645aaa5
chenliujin <liujin.chen@qq.com>
2016-09-08 07:58:52 +0800

    update

--- src/includes/templates/template_default/templates/tpl_checkout_shipping_default.php
+++ src/includes/templates/template_default/templates/tpl_checkout_shipping_default.php
@@ -32,106 +32,90 @@
 <br class="clearBoth" />
  
 <?php
-  if (zen_count_shipping_modules() > 0) {
-?>
+if ( !empty($quotes) ) { 
+	?>
  
-<h2 id="checkoutShippingHeadingMethod"><?php echo TABLE_HEADING_SHIPPING_METHOD; ?></h2>
+	<h2 id="checkoutShippingHeadingMethod"><?php echo TABLE_HEADING_SHIPPING_METHOD; ?></h2>
  
-<?php
-    if (sizeof($quotes) > 1 && sizeof($quotes[0]) > 1) {
-?>
- 
-<div id="checkoutShippingContentChoose" class="important"><?php echo TEXT_CHOOSE_SHIPPING_METHOD; ?></div>
- 
-<?php
-    } elseif ($free_shipping == false) {
-?>
-<div id="checkoutShippingContentChoose" class="important"><?php echo TEXT_ENTER_SHIPPING_INFORMATION; ?></div>
- 
-<?php
+	<?php
+	if (sizeof($quotes) > 1 && sizeof($quotes[0]) > 1) { 
+		?> 
+			<div id="checkoutShippingContentChoose" class="important"><?php echo TEXT_CHOOSE_SHIPPING_METHOD; ?></div> 
+		<?php
+	} elseif ($free_shipping == false) { 
+		?>
+			<div id="checkoutShippingContentChoose" class="important"><?php echo TEXT_ENTER_SHIPPING_INFORMATION; ?></div> 
+		<?php
     }
-?>
-<?php
+
     if ($free_shipping == true) { ?>
 		<div id="freeShip" class="important" ><?php echo FREE_SHIPPING_TITLE; ?>&nbsp;<?php echo $quotes[$i]['icon']; ?></div>
 		<div id="defaultSelected"><?php echo sprintf(FREE_SHIPPING_DESCRIPTION, $currencies->format(MODULE_ORDER_TOTAL_SHIPPING_FREE_SHIPPING_OVER)) . zen_draw_hidden_field('shipping', 'free_free'); ?></div> <?php
     } else {
-      $radio_buttons = 0;
-      for ($i=0, $n=sizeof($quotes); $i<$n; $i++) {
-      // bof: field set
-// allows FedEx to work comment comment out Standard and Uncomment FedEx
-//      if ($quotes[$i]['id'] != '' || $quotes[$i]['module'] != '') { // FedEx
-      if ($quotes[$i]['module'] != '') { // Standard
+		$radio_buttons = 0;
+		for ($i=0, $n=sizeof($quotes); $i<$n; $i++) {
+			// bof: field set
+			// allows FedEx to work comment comment out Standard and Uncomment FedEx
+			//      if ($quotes[$i]['id'] != '' || $quotes[$i]['module'] != '') { // FedEx
+			if ($quotes[$i]['module'] != '') { // Standard
+				?>
+				<fieldset>
+				<legend><?php echo $quotes[$i]['module']; ?>&nbsp;<?php if (isset($quotes[$i]['icon']) && zen_not_null($quotes[$i]['icon'])) { echo $quotes[$i]['icon']; } ?></legend>
+				<?php
+				if (isset($quotes[$i]['error'])) { 
+					?>
+						<div><?php echo $quotes[$i]['error']; ?></div>
+					<?php
+				} else {
+					for ($j=0, $n2=sizeof($quotes[$i]['methods']); $j<$n2; $j++) {
+						// set the radio button to be checked if it is the method chosen
+						$checked = FALSE;
+						if (isset($_SESSION['shipping']) && isset($_SESSION['shipping']['id'])) {
+							$checked = ($quotes[$i]['id'] . '_' . $quotes[$i]['methods'][$j]['id'] == $_SESSION['shipping']['id']);
+						}
+						if ( ($checked == true) || ($n == 1 && $n2 == 1) ) {
+							//echo '      <div id="defaultSelected" class="moduleRowSelected">' . "\n";
+							//} else {
+							//echo '      <div class="moduleRow">' . "\n";
+						}
+
+						if ( ($n > 1) || ($n2 > 1) ) { ?>
+							<div class="important forward"><?php echo $currencies->format(zen_add_tax($quotes[$i]['methods'][$j]['cost'], (isset($quotes[$i]['tax']) ? $quotes[$i]['tax'] : 0))); ?></div> <?php 
+						} else { ?>
+						<div class="important forward"><?php echo $currencies->format(zen_add_tax($quotes[$i]['methods'][$j]['cost'], $quotes[$i]['tax'])) . zen_draw_hidden_field('shipping', $quotes[$i]['id'] . '_' . $quotes[$i]['methods'][$j]['id']); ?></div> <?php 
+						}
+
+						echo zen_draw_radio_field(
+							'shipping', 
+							$quotes[$i]['id'] . '_' . $quotes[$i]['methods'][$j]['id'], 
+							$checked, 
+							'id="ship-'.$quotes[$i]['id'] . '-' . str_replace(' ', '-', $quotes[$i]['methods'][$j]['id']) .'"'
+						); 
+						?>
+
+						<label for="ship-<?php echo $quotes[$i]['id'] . '-' . str_replace(' ', '-', $quotes[$i]['methods'][$j]['id']); ?>" class="checkboxLabel" ><?php echo $quotes[$i]['methods'][$j]['title']; ?></label>
+						<!--</div>-->
+						<br class="clearBoth" /> <?php
+						$radio_buttons++;
+					}
+				} ?> 
+				</fieldset> <?php
+			}
+			// eof: field set
+		}
+	}
+
+
+} else { ?>
+	<h2 id="checkoutShippingHeadingMethod"><?php echo TITLE_NO_SHIPPING_AVAILABLE; ?></h2>
+	<div id="checkoutShippingContentChoose" class="important"><?php echo TEXT_NO_SHIPPING_AVAILABLE; ?></div><?php
+}
 ?>
-<fieldset>
-<legend><?php echo $quotes[$i]['module']; ?>&nbsp;<?php if (isset($quotes[$i]['icon']) && zen_not_null($quotes[$i]['icon'])) { echo $quotes[$i]['icon']; } ?></legend>
- 
-<?php
-        if (isset($quotes[$i]['error'])) {
-?>
-      <div><?php echo $quotes[$i]['error']; ?></div>
-<?php
-        } else {
-          for ($j=0, $n2=sizeof($quotes[$i]['methods']); $j<$n2; $j++) {
-// set the radio button to be checked if it is the method chosen
-            $checked = FALSE;
-            if (isset($_SESSION['shipping']) && isset($_SESSION['shipping']['id'])) {
-              $checked = ($quotes[$i]['id'] . '_' . $quotes[$i]['methods'][$j]['id'] == $_SESSION['shipping']['id']);
-            }
-            if ( ($checked == true) || ($n == 1 && $n2 == 1) ) {
-              //echo '      <div id="defaultSelected" class="moduleRowSelected">' . "\n";
-            //} else {
-              //echo '      <div class="moduleRow">' . "\n";
-            }
-?>
-<?php
-            if ( ($n > 1) || ($n2 > 1) ) {
-?>
-<div class="important forward"><?php echo $currencies->format(zen_add_tax($quotes[$i]['methods'][$j]['cost'], (isset($quotes[$i]['tax']) ? $quotes[$i]['tax'] : 0))); ?></div>
-<?php
-            } else {
-?>
-<div class="important forward"><?php echo $currencies->format(zen_add_tax($quotes[$i]['methods'][$j]['cost'], $quotes[$i]['tax'])) . zen_draw_hidden_field('shipping', $quotes[$i]['id'] . '_' . $quotes[$i]['methods'][$j]['id']); ?></div>
-<?php
-            }
-?>
- 
-<?php 
-			echo zen_draw_radio_field(
-				'shipping', 
-				$quotes[$i]['id'] . '_' . $quotes[$i]['methods'][$j]['id'], 
-				$checked, 
-				'id="ship-'.$quotes[$i]['id'] . '-' . str_replace(' ', '-', $quotes[$i]['methods'][$j]['id']) .'"'
-			); 
-?>
-<label for="ship-<?php echo $quotes[$i]['id'] . '-' . str_replace(' ', '-', $quotes[$i]['methods'][$j]['id']); ?>" class="checkboxLabel" ><?php echo $quotes[$i]['methods'][$j]['title']; ?></label>
-<!--</div>-->
-<br class="clearBoth" />
-<?php
-            $radio_buttons++;
-          }
-        }
-?>
- 
-</fieldset>
-<?php
-    }
-// eof: field set
-      }
-    }
-?>
- 
-<?php
-  } else {
-?>
-<h2 id="checkoutShippingHeadingMethod"><?php echo TITLE_NO_SHIPPING_AVAILABLE; ?></h2>
-<div id="checkoutShippingContentChoose" class="important"><?php echo TEXT_NO_SHIPPING_AVAILABLE; ?></div>
-<?php
-  }
-?>
+
+
 <fieldset class="shipping" id="comments">
-<legend><?php echo TABLE_HEADING_COMMENTS; ?></legend>
-<?php echo zen_draw_textarea_field('comments', '45', '3'); ?>
+	<legend><?php echo TABLE_HEADING_COMMENTS; ?></legend>
+	<?php echo zen_draw_textarea_field('comments', '45', '3'); ?>
 </fieldset>
  
 <div class="buttonRow forward"><?php echo zen_image_submit(BUTTON_IMAGE_CONTINUE_CHECKOUT, BUTTON_CONTINUE_ALT); ?></div>
