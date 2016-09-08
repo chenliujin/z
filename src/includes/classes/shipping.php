3dc7d44c1a47b36267d8be0eb6c4b06f496d92fa
chenliujin <liujin.chen@qq.com>
2016-09-08 08:35:12 +0800

    update

--- src/includes/classes/shipping.php
+++ src/includes/classes/shipping.php
@@ -20,58 +20,17 @@ include_once('z/model/transportation_zone.php');
  *
  * @package classes
  */
-class shipping extends base {
+class shipping extends base 
+{
 	var $modules;
 
-	// class constructor
-	function __construct($module = '') {
-		global $PHP_SELF, $messageStack;
-
-		if (defined('MODULE_SHIPPING_INSTALLED') && zen_not_null(MODULE_SHIPPING_INSTALLED)) {
-			$this->modules = explode(';', MODULE_SHIPPING_INSTALLED);
-
-			$include_modules = array();
-
-			if ( (zen_not_null($module)) && (in_array(substr($module['id'], 0, strpos($module['id'], '_')) . '.' . substr($PHP_SELF, (strrpos($PHP_SELF, '.')+1)), $this->modules)) ) {
-				$include_modules[] = array(
-					'class' => substr($module['id'], 0, strpos($module['id'], '_')), 
-					'file' => substr($module['id'], 0, strpos($module['id'], '_')) . '.' . substr($PHP_SELF, (strrpos($PHP_SELF, '.')+1)));
-			} else {
-				reset($this->modules);
-				while (list(, $value) = each($this->modules)) {
-					$class = substr($value, 0, strrpos($value, '.'));
-					$include_modules[] = array(
-						'class' => $class, 
-						'file' => $value
-					);
-				}
-			}
-
-			for ($i=0, $n=sizeof($include_modules); $i<$n; $i++) {
-				$lang_file = null;
-				$module_file = DIR_WS_MODULES . 'shipping/' . $include_modules[$i]['file'];
-				if (IS_ADMIN_FLAG === true) {
-					$lang_file = zen_get_file_directory(DIR_FS_CATALOG . DIR_WS_LANGUAGES . $_SESSION['language'] . '/modules/shipping/', $include_modules[$i]['file'], 'false');
-					$module_file = DIR_FS_CATALOG . $module_file;
-				} else {
-					$lang_file = zen_get_file_directory(DIR_WS_LANGUAGES . $_SESSION['language'] . '/modules/shipping/', $include_modules[$i]['file'], 'false');
-				}
-				if (@file_exists($lang_file)) {
-					include_once($lang_file);
-				} else {
-					if (IS_ADMIN_FLAG === false && is_object($messageStack)) {
-						$messageStack->add('checkout_shipping', WARNING_COULD_NOT_LOCATE_LANG_FILE . $lang_file, 'caution');
-					} else {
-						$messageStack->add_session(WARNING_COULD_NOT_LOCATE_LANG_FILE . $lang_file, 'caution');
-					}
-				}
-
-				$this->enabled = TRUE;
-
-				include_once($module_file);
-				$GLOBALS[$include_modules[$i]['class']] = new $include_modules[$i]['class'];
-			}
-		}
+	/**
+	 * @author chenliujin <liujin.chen@qq.com>
+	 * @since 2016-09-08
+	 */
+	public function __construct($module = '') 
+	{
+		if ($module) $this->module = $module; 
 	}
 
 	/**
@@ -85,6 +44,7 @@ class shipping extends base {
 		$params = ['status' => 1];
 
 		if ($module) $params['code'] = $module;
+		if ($this->module) $params['code'] = $this->module; 
 
 		$transportation 		= new \z\transportation;
 		$transportation_list 	= $transportation->findAll($params);
@@ -100,45 +60,46 @@ class shipping extends base {
 		return $quotes_array;
 	}
 
-	function cheapest() {
-		if (is_array($this->modules)) {
-			$rates = array();
+	/**
+	 * @author chenliujin <liujin.chen@qq.com>
+	 * @since 2016-09-08
+	 */
+	public function cheapest() 
+	{
+		$rates = array();
 
-			reset($this->modules);
-			while (list(, $value) = each($this->modules)) {
-				$class = substr($value, 0, strrpos($value, '.'));
-				if ($GLOBALS[$class]->enabled) {
-					$quotes = $GLOBALS[$class]->quotes;
-					$size = sizeof($quotes['methods']);
-					for ($i=0; $i<$size; $i++) {
-						//              if ($quotes['methods'][$i]['cost']) {
-						if (isset($quotes['methods'][$i]['cost'])){
-							$rates[] = array('id' => $quotes['id'] . '_' . $quotes['methods'][$i]['id'],
-								'title' => $quotes['module'] . ' (' . $quotes['methods'][$i]['title'] . ')',
-								'cost' => $quotes['methods'][$i]['cost'],
-								'module' => $quotes['id']
-							);
-						}
-					}
-				}
-			}
+		$list = $this->quote();
 
-			$cheapest = false;
-			$size = sizeof($rates);
+		foreach ($list as $quotes) {
+			$size = sizeof($quotes['methods']);
 			for ($i=0; $i<$size; $i++) {
-				if (is_array($cheapest)) {
-					// never quote storepickup as lowest - needs to be configured in shipping module
-					if ($rates[$i]['cost'] < $cheapest['cost'] and $rates[$i]['module'] != 'storepickup') {
-						$cheapest = $rates[$i];
-					}
-				} else {
-					if ($rates[$i]['module'] != 'storepickup') {
-						$cheapest = $rates[$i];
-					}
+				if (isset($quotes['methods'][$i]['cost'])){
+					$rates[] = array(
+						'id' 		=> $quotes['id'] . '_' . $quotes['methods'][$i]['id'],
+						'title' 	=> $quotes['module'] . ' (' . $quotes['methods'][$i]['title'] . ')',
+						'cost' 		=> $quotes['methods'][$i]['cost'],
+						'module' 	=> $quotes['id']
+					);
 				}
 			}
-			$this->notify('NOTIFY_SHIPPING_MODULE_CALCULATE_CHEAPEST', $cheapest, $cheapest, $rates);
-			return $cheapest;
+
 		}
+
+		$cheapest = false;
+		$size = sizeof($rates);
+		for ($i=0; $i<$size; $i++) {
+			if (is_array($cheapest)) {
+				// never quote storepickup as lowest - needs to be configured in shipping module
+				if ($rates[$i]['cost'] < $cheapest['cost'] and $rates[$i]['module'] != 'storepickup') {
+					$cheapest = $rates[$i];
+				}
+			} else {
+				if ($rates[$i]['module'] != 'storepickup') {
+					$cheapest = $rates[$i];
+				}
+			}
+		}
+		$this->notify('NOTIFY_SHIPPING_MODULE_CALCULATE_CHEAPEST', $cheapest, $cheapest, $rates);
+		return $cheapest;
 	}
 }
