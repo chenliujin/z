96392d1fc83ccdb9b55a6b104a11fbe5d679de77
chenliujin <liujin.chen@qq.com>
2016-09-11 20:31:34 +0800

    update

--- src/includes/languages/english/shopping_cart.php
+++ src/includes/languages/english/shopping_cart.php
@@ -1,16 +1,8 @@
 <?php
-/**
- * @package languageDefines
- * @copyright Copyright 2003-2016 Zen Cart Development Team
- * @copyright Portions Copyright 2003 osCommerce
- * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: Author: DrByte  Fri Sep 11 15:56:13 2015 -0400 Modified in v1.5.5 $
- */
-
 define('TEXT_INFORMATION', 'You may proceed with your purchase by clicking the Checkout button below. Shipping and Taxes and Discounts will be handled on subsequent pages.');
 
-define('NAVBAR_TITLE', 'The Shopping Cart');
-define('HEADING_TITLE', 'Your Shopping Cart Contents');
+define('NAVBAR_TITLE', 'Shopping Cart');
+define('HEADING_TITLE', 'Shopping Cart');
 define('HEADING_TITLE_EMPTY', 'Your Shopping Cart');
 define('TABLE_HEADING_REMOVE', 'Remove');
 define('TABLE_HEADING_QUANTITY', 'Quantity');
