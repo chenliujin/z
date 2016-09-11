dc2f535fdcfc76ab89fd2b56d5597815307d70df
chenliujin <liujin.chen@qq.com>
2016-09-11 18:50:47 +0800

    update

--- src/includes/languages/english/shopping_cart.php
+++ src/includes/languages/english/shopping_cart.php
@@ -15,7 +15,7 @@ define('HEADING_TITLE_EMPTY', 'Your Shopping Cart');
 define('TABLE_HEADING_REMOVE', 'Remove');
 define('TABLE_HEADING_QUANTITY', 'Quantity');
 define('TABLE_HEADING_MODEL', 'Model');
-define('TABLE_HEADING_PRICE','Unit');
+define('TABLE_HEADING_PRICE','Price');
 define('TEXT_CART_EMPTY', 'Your Shopping Cart is empty.');
 define('SUB_TITLE_SUB_TOTAL', 'Subtotal:');
 define('SUB_TITLE_TOTAL', 'Total:');
