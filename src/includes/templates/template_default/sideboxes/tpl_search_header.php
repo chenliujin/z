da94b8cc7a8106d4b424ee2239c1f46dd3107143
chenliujin <liujin.chen@qq.com>
2016-09-26 12:07:17 +0800

    update

--- src/includes/templates/template_default/sideboxes/tpl_search_header.php
+++ src/includes/templates/template_default/sideboxes/tpl_search_header.php
@@ -4,18 +4,7 @@ $content .= zen_draw_form('quick_find_header', zen_href_link(FILENAME_ADVANCED_S
 $content .= zen_draw_hidden_field('main_page',FILENAME_ADVANCED_SEARCH_RESULT);
 $content .= zen_draw_hidden_field('search_in_description', '1') . zen_hide_session_id();
 
-if (strtolower(IMAGE_USE_CSS_BUTTONS) == 'yes') {
-	$content .= zen_draw_input_field(
-		'keyword', 
-		'', 
-		'size="6" maxlength="30" style="width: 100px"'
-		) . zen_image_submit(BUTTON_IMAGE_SEARCH, HEADER_SEARCH_BUTTON);
-} else {
-	$content .= zen_draw_input_field(
-		'keyword', 
-		'', 
-		'size="6" maxlength="30" style="width: 100px"'
-		) . '<input type="submit" value="' . HEADER_SEARCH_BUTTON . '" style="width: 45px" />';
-}
+$content .= zen_draw_input_field('keyword', '', 'size="6" maxlength="30" style="width: 100px"');
+$content .= zen_image_submit(BUTTON_IMAGE_SEARCH, HEADER_SEARCH_BUTTON);
 
 $content .= "</form>";
