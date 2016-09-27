6f2dd6ceffca4c92b018aedb1d53cf8f2dc1fc08
chenliujin <liujin.chen@qq.com>
2016-09-27 11:02:56 +0800

    format

--- src/includes/classes/db/mysql/define_queries.php
+++ src/includes/classes/db/mysql/define_queries.php
@@ -1,17 +1,8 @@
 <?php
-/**
- * define_queries
- * defines queries used in various codeblocks
- * can be used to assist with special requirements for other database-abstraction configurations
- *
- * @package classes
- * @copyright Copyright 2003-2016 Zen Cart Development Team
- * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: Author: DrByte  Fri Jan 1 01:37:09 2016 -0500 Modified in v1.5.5 $
- */
 if (!defined('IS_ADMIN_FLAG')) {
   die('Illegal Access');
 }
+
 DEFINE('SQL_CC_ENABLED', "select configuration_key from " . TABLE_CONFIGURATION . " where configuration_key RLIKE 'CC_ENABLED' and configuration_value= '1'");
 DEFINE('SQL_SHOW_PRODUCT_INFO_CATEGORY', "select configuration_key, configuration_value from " . TABLE_CONFIGURATION . " where configuration_key RLIKE 'SHOW_PRODUCT_INFO_CATEGORY' and configuration_value > 0 order by configuration_value");
 DEFINE('SQL_SHOW_PRODUCT_INFO_MAIN',"select configuration_key, configuration_value from " . TABLE_CONFIGURATION . " where configuration_key RLIKE 'SHOW_PRODUCT_INFO_MAIN' and configuration_value > 0 order by configuration_value");
