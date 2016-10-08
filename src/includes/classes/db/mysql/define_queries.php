320d8d034943068e4c2bd006e54909bd66928e7a
chenliujin <liujin.chen@qq.com>
2016-10-08 23:16:51 +0800

    update

--- src/includes/classes/db/mysql/define_queries.php
+++ src/includes/classes/db/mysql/define_queries.php
@@ -6,12 +6,6 @@ if (!defined('IS_ADMIN_FLAG')) {
 DEFINE('SQL_CC_ENABLED', "select configuration_key from " . TABLE_CONFIGURATION . " where configuration_key RLIKE 'CC_ENABLED' and configuration_value= '1'");
 DEFINE('SQL_SHOW_PRODUCT_INFO_CATEGORY', "select configuration_key, configuration_value from " . TABLE_CONFIGURATION . " where configuration_key RLIKE 'SHOW_PRODUCT_INFO_CATEGORY' and configuration_value > 0 order by configuration_value");
 
-DEFINE('SQL_SHOW_PRODUCT_INFO_MAIN', "
-	select configuration_key, configuration_value 
-	from " . TABLE_CONFIGURATION . " 
-	where configuration_key RLIKE 'SHOW_PRODUCT_INFO_MAIN' and configuration_value > 0 
-	order by configuration_value");
-
 DEFINE('SQL_SHOW_PRODUCT_INFO_MISSING',"select configuration_key, configuration_value from " . TABLE_CONFIGURATION  . " where configuration_key RLIKE 'SHOW_PRODUCT_INFO_MISSING' and configuration_value > 0 order by configuration_value");
 DEFINE('SQL_SHOW_PRODUCT_INFO_LISTING_BELOW',"select configuration_key, configuration_value from " . TABLE_CONFIGURATION . " where configuration_key RLIKE 'SHOW_PRODUCT_INFO_LISTING_BELOW' and configuration_value > 0 order by configuration_value");
 DEFINE('SQL_BANNER_CHECK_QUERY', "select count(*) as count from " . TABLE_BANNERS_HISTORY . "                where banners_id = '%s' and date_format(banners_history_date, '%%Y%%m%%d') = date_format(now(), '%%Y%%m%%d')");
--- src/includes/templates/template_default/templates/tpl_index_default.php
+++ src/includes/templates/template_default/templates/tpl_index_default.php
@@ -1,27 +1,3 @@
 <div class="centerColumn" id="indexDefault">
-
-<?php
-
-$show_display_category = $db->Execute(SQL_SHOW_PRODUCT_INFO_MAIN);
-while (!$show_display_category->EOF) {
-	if ($show_display_category->fields['configuration_key'] == 'SHOW_PRODUCT_INFO_MAIN_FEATURED_PRODUCTS') { 
-		require($template->get_template_dir('tpl_modules_featured_products.php',DIR_WS_TEMPLATE, $current_page_base,'templates'). '/tpl_modules_featured_products.php'); 
-	} 
-
-	if ($show_display_category->fields['configuration_key'] == 'SHOW_PRODUCT_INFO_MAIN_SPECIALS_PRODUCTS') { 
-		require($template->get_template_dir('tpl_modules_specials_default.php',DIR_WS_TEMPLATE, $current_page_base,'templates'). '/tpl_modules_specials_default.php'); 
-	} 
-
-	if ($show_display_category->fields['configuration_key'] == 'SHOW_PRODUCT_INFO_MAIN_NEW_PRODUCTS') { 
-		require($template->get_template_dir('tpl_modules_whats_new.php',DIR_WS_TEMPLATE, $current_page_base,'templates'). '/tpl_modules_whats_new.php'); 
-	} 
-
-	if ($show_display_category->fields['configuration_key'] == 'SHOW_PRODUCT_INFO_MAIN_UPCOMING') { 
-		include(DIR_WS_MODULES . zen_get_module_directory(FILENAME_UPCOMING_PRODUCTS)); 
-	} 
-
-	$show_display_category->MoveNext();
-} 
-?>
-
+TODO
 </div>
