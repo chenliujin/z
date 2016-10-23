4d4fafe982d0958d9ed59aed09dca135f848fc65
chenliujin <liujin.chen@qq.com>
2016-10-23 18:27:59 +0800

    format

--- src/includes/templates/responsive_classic/templates/tpl_index_product_list.php
+++ src/includes/templates/responsive_classic/templates/tpl_index_product_list.php
@@ -1,17 +1,3 @@
-<?php
-/**
- * Page Template
- *
- * Loaded by main_page=index<br />
- * Displays product-listing when a particular category/subcategory is selected for browsing
- *
- * @package templateSystem
- * @copyright Copyright 2003-2016 Zen Cart Development Team
- * @copyright Portions Copyright 2003 osCommerce
- * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: picaflor-azul Wed Jan 13 18:44:28 2016 -0500 New in v1.5.5 $
- */
-?>
 <div class="centerColumn" id="indexProductList">
 
 <div id="cat-top" class="group">
@@ -99,14 +85,9 @@ require(DIR_WS_MODULES . zen_get_module_directory(FILENAME_PRODUCT_LISTING_ALPHA
 <?php // end wrapper ?>
 <?php if ($listing->RecordCount()) { ?>
 </div>
-<?php } ?>
+<?php } 
 
-
-<?php
-/**
- * require the code for listing products
- */
- require($template->get_template_dir('tpl_modules_product_listing.php', DIR_WS_TEMPLATE, $current_page_base,'templates'). '/' . 'tpl_modules_product_listing.php');
+	require($template->get_template_dir('tpl_modules_product_listing.php', DIR_WS_TEMPLATE, $current_page_base,'templates'). '/' . 'tpl_modules_product_listing.php');
 ?>
 
 
