0af8c6cecdff8d381fceab489375a64be1ea8cb6
chenliujin <liujin.chen@qq.com>
2016-10-28 14:55:58 +0800

    format

--- src/includes/templates/responsive_classic/common/tpl_columnar_display.php
+++ src/includes/templates/responsive_classic/common/tpl_columnar_display.php
@@ -1,33 +1,18 @@
 <?php
-$zco_notifier->notify('NOTIFY_TPL_COLUMNAR_DISPLAY_START', $current_page_base, $list_box_contents, $title);
-?>
-<?php
 if ($title) {
-?>
-<?php echo $title; ?>
-<?php
+	echo $title; 
 }
-?>
-<?php
+
 if (is_array($list_box_contents) > 0 ) {
 	for($row=0;$row<sizeof($list_box_contents);$row++) {
 		$params = "";
-?>
 
-<?php
 		for($col=0;$col<sizeof($list_box_contents[$row]);$col++) {
 			$r_params = "";
 			if (isset($list_box_contents[$row][$col]['params'])) $r_params .= ' ' . (string)$list_box_contents[$row][$col]['params'];
-			if (isset($list_box_contents[$row][$col]['text'])) {
-?>
-	<?php echo '<div' . $r_params . '>' . $list_box_contents[$row][$col]['text'] .  '</div>' . "\n"; ?>
-<?php
+			if (isset($list_box_contents[$row][$col]['text'])) { 
+				echo '<div' . $r_params . '>' . $list_box_contents[$row][$col]['text'] .  '</div>' . "\n";
 			}
-		}
-?>
-
-<?php
+		} 
 	}
 }
-
-$zco_notifier->notify('NOTIFY_TPL_COLUMNAR_DISPLAY_END', $current_page_base, $list_box_contents, $title);
