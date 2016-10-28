c207249015e78bef94c6b36b93554ef6ff46fa7a
chenliujin <liujin.chen@qq.com>
2016-10-28 15:09:20 +0800

    format

--- src/includes/templates/responsive_classic/common/tpl_tabular_display.php
+++ src/includes/templates/responsive_classic/common/tpl_tabular_display.php
@@ -18,9 +18,18 @@ for($row=0; $row<sizeof($list_box_contents); $row++) {
 	for($col=0; $col<sizeof($list_box_contents[$row]); $col++) {
 		$c_params = "";
 		$cell_type = ($row==0) ? 'li' : 'div';
-		if (isset($list_box_contents[$row][$col]['params'])) $c_params .= ' ' . $list_box_contents[$row][$col]['params'];
-		if (isset($list_box_contents[$row][$col]['align']) && $list_box_contents[$row][$col]['align'] != '') $c_params .= ' align="' . $list_box_contents[$row][$col]['align'] . '"';
-		if ($cell_type=='th') $c_params .= ' scope="' . $cell_scope . '" id="' . $cell_title . 'Cell' . $row . '-' . $col.'"';
+
+		if (isset($list_box_contents[$row][$col]['params'])) {
+			$c_params .= ' ' . $list_box_contents[$row][$col]['params'];
+		}
+
+		if (isset($list_box_contents[$row][$col]['align']) && $list_box_contents[$row][$col]['align'] != '') {
+			$c_params .= ' align="' . $list_box_contents[$row][$col]['align'] . '"';
+		}
+
+		if ($cell_type=='th') {
+			$c_params .= ' scope="' . $cell_scope . '" id="' . $cell_title . 'Cell' . $row . '-' . $col.'"';
+		}
 
 		if (isset($list_box_contents[$row][$col]['text'])) {
 			echo $list_box_contents[$row][$col]['text'];
