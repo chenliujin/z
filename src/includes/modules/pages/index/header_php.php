bcff28c7e56b6edad37fecaa071365dddd85d2af
chenliujin <liujin.chen@qq.com>
2016-10-28 15:33:23 +0800

    format

--- src/includes/modules/pages/index/header_php.php
+++ src/includes/modules/pages/index/header_php.php
@@ -2,20 +2,22 @@
 // the following cPath references come from application_top/initSystem
 $category_depth = 'top';
 if (isset($cPath) && zen_not_null($cPath)) {
-	$categories_products_query = "SELECT count(*) AS total
+	$categories_products_query = "
+		SELECT count(*) AS total
 		FROM   " . TABLE_PRODUCTS_TO_CATEGORIES . "
-		WHERE   categories_id = :categoriesID";
-
+		WHERE   categories_id = :categoriesID
+		";
 	$categories_products_query = $db->bindVars($categories_products_query, ':categoriesID', $current_category_id, 'integer');
 	$categories_products = $db->Execute($categories_products_query);
 
 	if ($categories_products->fields['total'] > 0) {
-		$category_depth = 'products'; // display products
+		$category_depth = 'products';
 	} else {
-		$category_parent_query = "SELECT count(*) AS total
+		$category_parent_query = "
+			SELECT count(*) AS total
 			FROM   " . TABLE_CATEGORIES . "
-			WHERE  parent_id = :categoriesID";
-
+			WHERE  parent_id = :categoriesID
+			";
 		$category_parent_query = $db->bindVars($category_parent_query, ':categoriesID', $current_category_id, 'integer');
 		$category_parent = $db->Execute($category_parent_query);
 
