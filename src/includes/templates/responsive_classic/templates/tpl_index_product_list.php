b363f87a5a5ed9b46f819e08f3be77e32f16e9f9
chenliujin <liujin.chen@qq.com>
2016-10-28 12:26:02 +0800

    update

--- src/includes/templates/responsive_classic/templates/tpl_index_product_list.php
+++ src/includes/templates/responsive_classic/templates/tpl_index_product_list.php
@@ -28,48 +28,37 @@ if ($current_categories_description != '') {
 </div>
 
 <?php 
-if ($listing->RecordCount()) { 
+if ($do_filter_list && $listing->RecordCount()) { 
 	?>
 <div id="filter-wrapper" class="group">
 	<?php 
-	$check_for_alpha = $listing_sql;
-	$check_for_alpha = $db->Execute($check_for_alpha);
-	
-	if ($do_filter_list) {
-		$form = zen_draw_form('filter', zen_href_link(FILENAME_DEFAULT), 'get') . '<label class="inputLabel">' .TEXT_SHOW . '</label>';
-		echo $form;
-		echo zen_draw_hidden_field('main_page', FILENAME_DEFAULT);
-	
-		// draw cPath if known
-		if (!$getoption_set) {
-			echo zen_draw_hidden_field('cPath', $cPath);
-		} else {
-			// draw manufacturers_id
-			echo zen_draw_hidden_field($get_option_variable, $_GET[$get_option_variable]);
-		}
-	
-		if (isset($_GET['music_genre_id']) && $_GET['music_genre_id'] != '') echo zen_draw_hidden_field('music_genre_id', $_GET['music_genre_id']);
-	
-		if (isset($_GET['record_company_id']) && $_GET['record_company_id'] != '') echo zen_draw_hidden_field('record_company_id', $_GET['record_company_id']);
-	
-		if (isset($_GET['typefilter']) && $_GET['typefilter'] != '') echo zen_draw_hidden_field('typefilter', $_GET['typefilter']);
-	
-		if ($get_option_variable != 'manufacturers_id' && isset($_GET['manufacturers_id']) && $_GET['manufacturers_id'] > 0) {
-			echo zen_draw_hidden_field('manufacturers_id', $_GET['manufacturers_id']);
-		}
-	
-		echo zen_draw_hidden_field('sort', $_GET['sort']);
-	
-		// draw filter_id (ie: category/mfg depending on $options)
-		if ($do_filter_list) {
-			echo zen_draw_pull_down_menu('filter_id', $options, (isset($_GET['filter_id']) ? $_GET['filter_id'] : ''), 'onchange="this.form.submit()"');
-		}
-	
-		?>
-	</form>
-		<?php
+	$form = zen_draw_form('filter', zen_href_link(FILENAME_DEFAULT), 'get') . '<label class="inputLabel">' .TEXT_SHOW . '</label>';
+	echo $form;
+	echo zen_draw_hidden_field('main_page', FILENAME_DEFAULT);
+
+	// draw cPath if known
+	if (!$getoption_set) {
+		echo zen_draw_hidden_field('cPath', $cPath);
+	} else {
+		// draw manufacturers_id
+		echo zen_draw_hidden_field($get_option_variable, $_GET[$get_option_variable]);
 	}
+
+	if (isset($_GET['music_genre_id']) && $_GET['music_genre_id'] != '') echo zen_draw_hidden_field('music_genre_id', $_GET['music_genre_id']);
+
+	if (isset($_GET['record_company_id']) && $_GET['record_company_id'] != '') echo zen_draw_hidden_field('record_company_id', $_GET['record_company_id']);
+
+	if (isset($_GET['typefilter']) && $_GET['typefilter'] != '') echo zen_draw_hidden_field('typefilter', $_GET['typefilter']);
+
+	if ($get_option_variable != 'manufacturers_id' && isset($_GET['manufacturers_id']) && $_GET['manufacturers_id'] > 0) {
+		echo zen_draw_hidden_field('manufacturers_id', $_GET['manufacturers_id']);
+	}
+
+	echo zen_draw_hidden_field('sort', $_GET['sort']);
+
+	echo zen_draw_pull_down_menu('filter_id', $options, (isset($_GET['filter_id']) ? $_GET['filter_id'] : ''), 'onchange="this.form.submit()"');
 	?>
+	</form>
 </div>
 	<?php 
 } 
