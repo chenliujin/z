e7f3dde1604cfd903b9e98048f48273b69a9a91b
chenliujin <liujin.chen@qq.com>
2016-10-28 09:30:40 +0800

    update

--- sh/rsync.sh
+++ sh/rsync.sh
@@ -2,6 +2,7 @@
 
 rsync --exclude "*.swp" -avz --exclude "zc_install" --delete ../src/images/ /data/www/z/images/
 rsync --exclude "*.swp" -avz --exclude "zc_install" --exclude "*configure.php" ../src/* /data/www/z/
+rsync --exclude "*.swp" -avz --exclude "zc_install" --exclude "*configure.php" --delete ../src/includes/* /data/www/z/includes/
 
 
 chmod a+w /data/www/z/logs
@@ -10,3 +11,4 @@ chmod a+w /data/www/z/images
 chmod a+w /data/www/z/includes/languages/english/html_includes
 chmod a+w /data/www/z/media
 chmod a+w /data/www/z/pub
+
