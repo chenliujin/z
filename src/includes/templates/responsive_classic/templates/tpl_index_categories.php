47814c32dfccd47b8862780d4be257524bf57ebe
chenliujin <liujin.chen@qq.com>
2016-11-07 16:23:07 +0800

    format

--- src/includes/templates/responsive_classic/templates/tpl_index_categories.php
+++ src/includes/templates/responsive_classic/templates/tpl_index_categories.php
@@ -5,7 +5,7 @@
 <?php if (DEFINE_MAIN_PAGE_STATUS >= 1 and DEFINE_MAIN_PAGE_STATUS <= 2) { ?>
 <div id="indexCategoriesMainContent" class="content">
 <?php
-	include($define_page);
+include($define_page);
 ?>
 </div>
 <?php } ?>
@@ -18,65 +18,17 @@
 <?php } 
 
 if ($show_welcome != true) { 
-	?>
+?>
 </div>
 </div>
-	<?php 
+<?php 
 } 
-?>
 
+if (PRODUCT_LIST_CATEGORY_ROW_STATUS == 0) {
+	// do nothing
+} else {
+	require($template->get_template_dir('tpl_modules_category_row.php',DIR_WS_TEMPLATE, $current_page_base,'templates'). '/tpl_modules_category_row.php');
+}
 
-<?php
-			if (PRODUCT_LIST_CATEGORY_ROW_STATUS == 0) {
-				// do nothing
-			} else {
-				// display subcategories
-				/**
-				 * require the code to display the sub-categories-grid, if any exist
-				 */
-				require($template->get_template_dir('tpl_modules_category_row.php',DIR_WS_TEMPLATE, $current_page_base,'templates'). '/tpl_modules_category_row.php');
-			}
-?>
-<?php
-		$show_display_category = $db->Execute(SQL_SHOW_PRODUCT_INFO_CATEGORY);
-
-		while (!$show_display_category->EOF) {
-			// //  echo 'I found ' . zen_get_module_directory(FILENAME_UPCOMING_PRODUCTS);
-
-?>
-
-<?php if ($show_display_category->fields['configuration_key'] == 'SHOW_PRODUCT_INFO_CATEGORY_FEATURED_PRODUCTS') { ?>
-<?php
-			/**
-			 * display the Featured Products Center Box
-			 */
-?>
-<?php require($template->get_template_dir('tpl_modules_featured_products.php',DIR_WS_TEMPLATE, $current_page_base,'templates'). '/tpl_modules_featured_products.php'); ?>
-<?php } ?>
-
-<?php if ($show_display_category->fields['configuration_key'] == 'SHOW_PRODUCT_INFO_CATEGORY_SPECIALS_PRODUCTS') { ?>
-<?php
-			/**
-			 * display the Special Products Center Box
-			 */
-?>
-<?php require($template->get_template_dir('tpl_modules_specials_default.php',DIR_WS_TEMPLATE, $current_page_base,'templates'). '/tpl_modules_specials_default.php'); ?>
-<?php } ?>
-
-<?php if ($show_display_category->fields['configuration_key'] == 'SHOW_PRODUCT_INFO_CATEGORY_NEW_PRODUCTS') { ?>
-<?php
-			/**
-			 * display the New Products Center Box
-			 */
-?>
-<?php require($template->get_template_dir('tpl_modules_whats_new.php',DIR_WS_TEMPLATE, $current_page_base,'templates'). '/tpl_modules_whats_new.php'); ?>
-<?php } ?>
-
-<?php if ($show_display_category->fields['configuration_key'] == 'SHOW_PRODUCT_INFO_CATEGORY_UPCOMING') { ?>
-<?php include(DIR_WS_MODULES . zen_get_module_directory(FILENAME_UPCOMING_PRODUCTS)); ?>
-<?php } ?>
-<?php
-			$show_display_category->MoveNext();
-		} // !EOF
 ?>
 </div>
