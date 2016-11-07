541c06d13ba43f29c471ba14ad51fd6afb93f6be
chenliujin <liujin.chen@qq.com>
2016-11-07 18:47:29 +0800

    format

--- src/includes/modules/pages/checkout_confirmation/header_php.php
+++ src/includes/modules/pages/checkout_confirmation/header_php.php
@@ -1,16 +1,4 @@
 <?php
-/**
- * checkout_confirmation header_php.php
- *
- * @package page
- * @copyright Copyright 2003-2016 Zen Cart Development Team
- * @copyright Portions Copyright 2003 osCommerce
- * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: Author: DrByte  Mon Dec 7 14:40:03 2015 -0500 Modified in v1.5.5 $
- */
-
-// This should be first line of the script:
-$zco_notifier->notify('NOTIFY_HEADER_START_CHECKOUT_CONFIRMATION');
 
 // if there is nothing in the customers cart, redirect them to the shopping cart page
 if ($_SESSION['cart']->count_contents() <= 0) {
@@ -168,6 +156,3 @@ if (isset(${$_SESSION['payment']}->flagDisablePaymentAddressChange)) {
 require(DIR_WS_MODULES . zen_get_module_directory('require_languages.php'));
 $breadcrumb->add(NAVBAR_TITLE_1, zen_href_link(FILENAME_CHECKOUT_SHIPPING, '', 'SSL'));
 $breadcrumb->add(NAVBAR_TITLE_2);
-
-// This should be last line of the script:
-$zco_notifier->notify('NOTIFY_HEADER_END_CHECKOUT_CONFIRMATION');
