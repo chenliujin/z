e708be1560156f72602e80bafd2f82b7c4baf136
chenliujin <liujin.chen@qq.com>
2016-11-07 20:53:40 +0800

    format

--- src/includes/modules/pages/checkout_confirmation/jscript_main.php
+++ src/includes/modules/pages/checkout_confirmation/jscript_main.php
@@ -1,14 +1,3 @@
-<?php
-/**
- * jscript_main
- *
- * @package page
- * @copyright Copyright 2003-2016 Zen Cart Development Team
- * @copyright Portions Copyright 2003 osCommerce
- * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: Author: DrByte  Wed Jan 6 12:47:43 2016 -0500 Modified in v1.5.5 $
- */
-?>
 <script type="text/javascript"><!--
 var submitter = null;
 function popupWindow(url) {
