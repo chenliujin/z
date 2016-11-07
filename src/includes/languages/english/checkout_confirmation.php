8a10c23fd91af7ac7796884fa855b9f804a4ea7c
chenliujin <liujin.chen@qq.com>
2016-11-07 23:46:53 +0800

    update

--- src/includes/languages/english/checkout_confirmation.php
+++ src/includes/languages/english/checkout_confirmation.php
@@ -6,7 +6,7 @@ define('HEADING_TITLE', 'Step 3 of 3 - Order Confirmation');
 
 define('HEADING_BILLING_ADDRESS', 'Billing/Payment Information');
 define('HEADING_DELIVERY_ADDRESS', 'Delivery/Shipping Information');
-define('HEADING_SHIPPING_METHOD', 'Shipping Method:');
+define('HEADING_SHIPPING_METHOD', 'Shipping Method');
 define('HEADING_PAYMENT_METHOD', 'Payment Method');
 define('HEADING_TAX', 'Tax');
 define('HEADING_ORDER_COMMENTS', 'Special Instructions or Order Comments');
