1ea40fdbc749aa498ad792105463e883c9f0079d
chenliujin <liujin.chen@qq.com>
2016-11-09 19:16:42 +0800

    update

--- src/includes/languages/english/checkout_shipping_address.php
+++ src/includes/languages/english/checkout_shipping_address.php
@@ -9,7 +9,7 @@ define('TABLE_HEADING_SHIPPING_ADDRESS', 'Shipping Address');
 define('TITLE_SHIPPING_ADDRESS', 'Current Shipping Address');
 
 define('TABLE_HEADING_ADDRESS_BOOK_ENTRIES', '...Or Choose From Your Address Book Entries');
-define('TITLE_PLEASE_SELECT', 'Change the Shipping Address for This Order');
+define('TITLE_PLEASE_SELECT', 'Add a new address');
 
 define('TABLE_HEADING_NEW_SHIPPING_ADDRESS', 'New Shipping Address');
 define('TEXT_CREATE_NEW_SHIPPING_ADDRESS', 'Please use the following form to create a new shipping address for use with this order.');
