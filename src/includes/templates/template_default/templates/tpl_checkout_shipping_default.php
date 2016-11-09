c56f5627016f200f640c0b9f06ca8219eb5a2424
chenliujin <liujin.chen@qq.com>
2016-11-09 11:22:24 +0800

    remove

--- src/includes/templates/template_default/templates/tpl_checkout_shipping_default.php
+++ src/includes/templates/template_default/templates/tpl_checkout_shipping_default.php
@@ -94,14 +94,6 @@ tr td {
 ?>
 
 
-<fieldset class="shipping" id="comments">
-	<legend><?php echo TABLE_HEADING_COMMENTS; ?></legend>
-	<div>
-		<?php echo zen_draw_textarea_field('comments', '45', '5'); ?>
-	</div>
-</fieldset>
- 
-
 <div class="buttonRow forward"><?php echo zen_image_submit(BUTTON_IMAGE_CONTINUE_CHECKOUT, BUTTON_CONTINUE_ALT); ?></div>
 <div class="buttonRow back"><?php echo '<strong>' . TITLE_CONTINUE_CHECKOUT_PROCEDURE . '</strong><br />' . TEXT_CONTINUE_CHECKOUT_PROCEDURE; ?></div>
  
