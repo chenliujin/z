d1777126802f5161a40016063afb1cdccd95caa2
chenliujin <liujin.chen@qq.com>
2016-11-09 19:11:58 +0800

    format

--- src/includes/templates/template_default/templates/tpl_modules_checkout_new_address.php
+++ src/includes/templates/template_default/templates/tpl_modules_checkout_new_address.php
@@ -1,16 +1,3 @@
-<?php
-/**
- * Module Template
- *
- * Allows entry of new addresses during checkout stages
- *
- * @package templateSystem
- * @copyright Copyright 2003-2016 Zen Cart Development Team
- * @copyright Portions Copyright 2003 osCommerce
- * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: rbarbour zcadditions.com Fri Feb 26 00:03:33 2016 -0500 Modified in v1.5.5 $
- */
-?>
 <div class="centerColumnModule" id="checkoutNewAddress">
 <fieldset>
 <legend><?php echo TITLE_PLEASE_SELECT; ?></legend>
