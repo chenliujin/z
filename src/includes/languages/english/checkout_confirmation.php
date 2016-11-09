1677a5bcb63be000f70990e3b5ec2f75ded16e32
chenliujin <liujin.chen@qq.com>
2016-11-10 01:11:28 +0800

    remove

--- src/includes/languages/english/checkout_confirmation.php
+++ src/includes/languages/english/checkout_confirmation.php
@@ -9,7 +9,6 @@ define('HEADING_SHIPPING_METHOD', 'Shipping Method');
 define('HEADING_PAYMENT_METHOD', 'Payment Method');
 define('HEADING_TAX', 'Tax');
 define('HEADING_ORDER_COMMENTS', 'Special Instructions or Order Comments');
-define('NO_COMMENTS_TEXT', 'None');
 define('TITLE_CONTINUE_CHECKOUT_PROCEDURE', '<strong>Final Step</strong>');
 define('TEXT_CONTINUE_CHECKOUT_PROCEDURE', '- continue to confirm your order. Thank you!');
 
