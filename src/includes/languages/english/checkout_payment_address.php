17324f4b184f745cc4753b4f70f37c1b9bebbd17
chenliujin <liujin.chen@qq.com>
2016-11-09 19:17:11 +0800

    update

--- src/includes/languages/english/checkout_payment_address.php
+++ src/includes/languages/english/checkout_payment_address.php
@@ -1,13 +1,4 @@
 <?php
-/**
- * @package languageDefines
- * @copyright Copyright 2003-2005 Zen Cart Development Team
- * @copyright Portions Copyright 2003 osCommerce
- * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: checkout_payment_address.php 2315 2005-11-07 08:41:46Z drbyte $
- */
-
-
 define('NAVBAR_TITLE_1', 'Checkout');
 define('NAVBAR_TITLE_2', 'Change Billing Address');
 
@@ -18,10 +9,9 @@ define('TEXT_SELECTED_PAYMENT_DESTINATION', 'This is the current billing address
 define('TITLE_PAYMENT_ADDRESS', 'Billing Address');
 
 define('TEXT_SELECT_OTHER_PAYMENT_DESTINATION', 'Please select the preferred billing address if the invoice to this order is to be delivered elsewhere.');
-define('TITLE_PLEASE_SELECT', 'Change the Billing Address for This Order');
+define('TITLE_PLEASE_SELECT', 'Add a new address');
 
 define('TABLE_HEADING_NEW_PAYMENT_ADDRESS', '...Or Choose From Your Address Book Entries');
 
 define('TITLE_CONTINUE_CHECKOUT_PROCEDURE', '<strong>Continue</strong>');
 define('TEXT_CONTINUE_CHECKOUT_PROCEDURE', '- to payment method.');
-?>
