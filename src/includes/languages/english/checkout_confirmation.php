f7a462714a6f30f58bd1ce2bfb539c5a3b329558
chenliujin <liujin.chen@qq.com>
2016-11-09 10:51:44 +0800

    remove

--- src/includes/languages/english/checkout_confirmation.php
+++ src/includes/languages/english/checkout_confirmation.php
@@ -4,13 +4,11 @@ define('NAVBAR_TITLE_2', 'Confirmation');
 
 define('HEADING_TITLE', 'Step 3 of 3 - Order Confirmation');
 
-define('HEADING_BILLING_ADDRESS', 'Billing/Payment Information');
 define('HEADING_DELIVERY_ADDRESS', 'Delivery/Shipping Information');
 define('HEADING_SHIPPING_METHOD', 'Shipping Method');
 define('HEADING_PAYMENT_METHOD', 'Payment Method');
 define('HEADING_TAX', 'Tax');
 define('HEADING_ORDER_COMMENTS', 'Special Instructions or Order Comments');
-// no comments entered
 define('NO_COMMENTS_TEXT', 'None');
 define('TITLE_CONTINUE_CHECKOUT_PROCEDURE', '<strong>Final Step</strong>');
 define('TEXT_CONTINUE_CHECKOUT_PROCEDURE', '- continue to confirm your order. Thank you!');
