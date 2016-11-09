f7a3f5c0597b3b8dc0196789db2504917226ebf0
chenliujin <liujin.chen@qq.com>
2016-11-09 19:43:56 +0800

    remove

--- src/includes/languages/english/header.php
+++ src/includes/languages/english/header.php
@@ -1,5 +1,4 @@
 <?php
-// header text in includes/header.php
 define('HEADER_TITLE_CREATE_ACCOUNT', 'Create Account');
 define('HEADER_TITLE_MY_ACCOUNT', 'My Account');
 define('HEADER_TITLE_CART_CONTENTS', 'Cart');
@@ -9,12 +8,10 @@ define('HEADER_TITLE_CATALOG', 'Home');
 define('HEADER_TITLE_LOGOFF', 'Sign Out');
 define('HEADER_TITLE_LOGIN', 'Sign In');
 
-// added defines for header alt and text
 define('HEADER_ALT_TEXT', 'Powered by Zen Cart :: The Art of E-Commerce');
 define('HEADER_LOGO_WIDTH', '192px');
 define('HEADER_LOGO_HEIGHT', '64px');
 define('HEADER_LOGO_IMAGE', 'logo.gif');
 
-// header Search Button/Box Search Button
 define('HEADER_SEARCH_BUTTON','Go');
 define('SEARCH_DEFAULT_TEXT', 'search here');
