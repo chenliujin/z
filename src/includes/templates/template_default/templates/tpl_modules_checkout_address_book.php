a6d6bb59217b53ad7c6696f7603ca926a12227a3
chenliujin <liujin.chen@qq.com>
2016-11-10 00:20:35 +0800

    update

--- src/includes/templates/template_default/templates/tpl_modules_checkout_address_book.php
+++ src/includes/templates/template_default/templates/tpl_modules_checkout_address_book.php
@@ -21,31 +21,13 @@ $addresses = $db->Execute($addresses_query);
 if (!$addresses->EOF) $radio_buttons = $addresses->recordCount();
 
 while (!$addresses->EOF) {
-	if ($addresses->fields['address_book_id'] == $_SESSION['sendto']) {
-		echo '<div id="defaultSelected" class="moduleRowSelected">' . "\n";
-	} else {
-		echo '<div class="moduleRow">' . "\n";
-	} 
-	?>
-	
-		<div class="back">
-			<?php echo zen_draw_radio_field('address', $addresses->fields['address_book_id'], ($addresses->fields['address_book_id'] == $_SESSION['sendto']), 'id="name-' . $addresses->fields['address_book_id'] . '"'); ?>
-		</div>
-
-		<div class="back">
-			<label for="name-<?php echo $addresses->fields['address_book_id']; ?>">
-				<?php echo zen_output_string_protected($addresses->fields['firstname'] . ' ' . $addresses->fields['lastname']); ?>
-			</label>
-		</div>
-
-	</div>
-
-	<br class="clearBoth" />
-
-	<address>
-		<?php echo zen_address_format(zen_get_address_format_id($addresses->fields['country_id']), $addresses->fields, true, ' ', '<br />'); ?>
-	</address>
-
+	?> 
+	<label for="name-<?php echo $addresses->fields['address_book_id']; ?>">
+		<?php echo zen_draw_radio_field('address', $addresses->fields['address_book_id'], ($addresses->fields['address_book_id'] == $_SESSION['sendto']), 'id="name-' . $addresses->fields['address_book_id'] . '"'); ?>
+		<address style="display: inline-block; vertical-align: middle; margin-left: 10px; margin-right: 20px">
+			<?php echo zen_address_format(zen_get_address_format_id($addresses->fields['country_id']), $addresses->fields, true, ' ', '<br />'); ?>
+		</address> 
+	</label>
 	<?php
 	$addresses->MoveNext();
 }
