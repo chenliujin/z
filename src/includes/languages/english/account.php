8ece916993f9b841ab1db3100a0e863f7c19de20
chenliujin <liujin.chen@qq.com>
2016-11-09 19:51:22 +0800

    remove

--- src/includes/languages/english/account.php
+++ src/includes/languages/english/account.php
@@ -1,12 +1,4 @@
 <?php
-/**
- * @package languageDefines
- * @copyright Copyright 2003-2006 Zen Cart Development Team
- * @copyright Portions Copyright 2003 osCommerce
- * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: account.php 3595 2006-05-07 06:39:23Z drbyte $
- */
-
 define('NAVBAR_TITLE', 'My Account');
 define('HEADING_TITLE', 'My Account Information');
 
@@ -22,7 +14,6 @@ define('TABLE_HEADING_VIEW', 'View');
 
 define('MY_ACCOUNT_TITLE', 'My Account');
 define('MY_ACCOUNT_INFORMATION', 'View or change my account information.');
-define('MY_ACCOUNT_ADDRESS_BOOK', 'View or change entries in my address book.');
 define('MY_ACCOUNT_PASSWORD', 'Change my account password.');
 
 define('MY_ORDERS_TITLE', 'My Orders');
@@ -31,4 +22,3 @@ define('MY_ORDERS_VIEW', 'View the orders I have made.');
 define('EMAIL_NOTIFICATIONS_TITLE', 'Email Notifications');
 define('EMAIL_NOTIFICATIONS_NEWSLETTERS', 'Subscribe or unsubscribe from newsletters.');
 define('EMAIL_NOTIFICATIONS_PRODUCTS', 'View or change my product notification list.');
-?>
