9d54bd99463ee4010d281a2278f66ef7b7513f62
chenliujin <liujin.chen@qq.com>
2016-11-09 15:49:41 +0800

    remove

--- src/includes/modules/payment/paypalwpp.php
+++ src/includes/modules/payment/paypalwpp.php
@@ -1,17 +1,6 @@
 <?php
-/**
- * paypalwpp.php payment module class for PayPal Express Checkout payment method
- *
- * @package paymentMethod
- * @copyright Copyright 2003-2016 Zen Cart Development Team
- * @copyright Portions Copyright 2003 osCommerce
- * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: Author: DrByte  Wed Mar 27 01:25:43 2016 -0500 Modified in v1.5.5 $
- */
-/**
- * load the communications layer code
- */
 require_once(DIR_FS_CATALOG . DIR_WS_MODULES . 'payment/paypal/paypal_curl.php');
+
 /**
  * the PayPal payment module with Express Checkout
  */
