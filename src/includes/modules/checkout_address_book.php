7f42627b86ac063f7c560e17befbf6bf4baa77fe
chenliujin <liujin.chen@qq.com>
2016-11-09 20:52:14 +0800

    format

--- src/includes/modules/checkout_address_book.php
+++ src/includes/modules/checkout_address_book.php
@@ -5,11 +5,19 @@ if (!defined('IS_ADMIN_FLAG')) {
 
 $radio_buttons = 0;
 
-$addresses_query = "select address_book_id, entry_firstname as firstname, entry_lastname as lastname,
-	entry_company as company, entry_street_address as street_address,
-	entry_suburb as suburb, entry_city as city, entry_postcode as postcode,
-	entry_state as state, entry_zone_id as zone_id,
-	entry_country_id as country_id
+$addresses_query = "
+	select 
+		address_book_id, 
+		entry_firstname as firstname, 
+		entry_lastname as lastname,
+		entry_company as company, 
+		entry_street_address as street_address,
+		entry_suburb as suburb, 
+		entry_city as city, 
+		entry_postcode as postcode,
+		entry_state as state, 
+		entry_zone_id as zone_id,
+		entry_country_id as country_id
 	from " . TABLE_ADDRESS_BOOK . "
 	where customers_id = '" . (int)$_SESSION['customer_id'] . "'";
 
