1464dd9a92e838dba2ae30968b8bd9163dfc8224
chenliujin <liujin.chen@qq.com>
2016-11-09 19:15:33 +0800

    format

--- src/includes/languages/english/checkout_shipping_address.php
+++ src/includes/languages/english/checkout_shipping_address.php
@@ -1,12 +1,4 @@
 <?php
-/**
- * @package languageDefines
- * @copyright Copyright 2003-2005 Zen Cart Development Team
- * @copyright Portions Copyright 2003 osCommerce
- * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: checkout_shipping_address.php 2315 2005-11-07 08:41:46Z drbyte $
- */
-
 define('NAVBAR_TITLE', 'Change Shipping Address');
 define('NAVBAR_TITLE_1', 'Checkout');
 define('NAVBAR_TITLE_2', 'Change Shipping Address');
@@ -28,5 +20,3 @@ define('TEXT_CONTINUE_CHECKOUT_PROCEDURE', '- to shipping method.');
 
 define('SET_AS_PRIMARY', 'Set as Primary Address');
 define('NEW_ADDRESS_TITLE', 'Enter new address');
-
-?>
