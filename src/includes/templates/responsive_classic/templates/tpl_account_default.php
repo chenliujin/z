d44beb8794334ce318b86a88930a87c04927f6bc
chenliujin <liujin.chen@qq.com>
2016-11-09 19:31:09 +0800

    remove

--- src/includes/templates/responsive_classic/templates/tpl_account_default.php
+++ src/includes/templates/responsive_classic/templates/tpl_account_default.php
@@ -1,18 +1,3 @@
-<?php
-/**
- * Page Template
- *
- * Loaded automatically by index.php?main_page=account.<br />
- * Displays previous orders and options to change various Customer Account settings
- *
- * @package templateSystem
- * @copyright Copyright 2003-2016 Zen Cart Development Team
- * @copyright Portions Copyright 2003 osCommerce
- * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: picaflor-azul Fri Jan 8 00:33:36 2016 -0500 New in v1.5.5 $
- */
-?>
-
 <div class="centerColumn group" id="accountDefault">
 
 <h1 id="accountDefaultHeading"><?php echo HEADING_TITLE; ?></h1>
