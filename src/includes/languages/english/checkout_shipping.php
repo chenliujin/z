743d7b18381a67511c6bc8f5d99a56d146eeced3
chenliujin <liujin.chen@qq.com>
2016-11-10 11:43:08 +0800

    remove

--- src/includes/languages/english/checkout_shipping.php
+++ src/includes/languages/english/checkout_shipping.php
@@ -12,9 +12,6 @@ define('TITLE_PLEASE_SELECT', 'Please Select');
 define('TITLE_NO_SHIPPING_AVAILABLE', 'Not Available At This Time');
 define('TEXT_NO_SHIPPING_AVAILABLE','<span class="alert">Sorry, we are not shipping to your region at this time.</span><br />Please contact us for alternate arrangements.');
 
-define('TITLE_CONTINUE_CHECKOUT_PROCEDURE', 'Continue to Step 2');
-define('TEXT_CONTINUE_CHECKOUT_PROCEDURE', '- choose your payment method.');
-
 define('FREE_SHIPPING_DESCRIPTION', 'Free shipping for orders over %s');
 
 define('ERROR_PLEASE_RESELECT_SHIPPING_METHOD', 'Your available shipping options have changed. Please re-select your desired shipping method.');
