339802221c8c75563514facc466fd409c287269c
chenliujin <liujin.chen@qq.com>
2016-11-10 11:34:03 +0800

    remove

--- src/includes/languages/english/checkout_confirmation.php
+++ src/includes/languages/english/checkout_confirmation.php
@@ -9,7 +9,5 @@ define('HEADING_SHIPPING_METHOD', 'Shipping Method');
 define('HEADING_PAYMENT_METHOD', 'Payment Method');
 define('HEADING_TAX', 'Tax');
 define('HEADING_ORDER_COMMENTS', 'Special Instructions or Order Comments');
-define('TITLE_CONTINUE_CHECKOUT_PROCEDURE', '<strong>Final Step</strong>');
-define('TEXT_CONTINUE_CHECKOUT_PROCEDURE', '- continue to confirm your order. Thank you!');
 
 define('OUT_OF_STOCK_CAN_CHECKOUT', 'Products marked with ' . STOCK_MARK_PRODUCT_OUT_OF_STOCK . ' are out of stock.<br />Items not in stock will be placed on backorder.');
