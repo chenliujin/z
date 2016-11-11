fed256cf2129b0c6f7cef9c28bc013ebc42651a5
chenliujin <liujin.chen@qq.com>
2016-11-11 19:57:33 +0800

    ga

--- src/includes/templates/responsive_classic/common/tpl_main_page.php
+++ src/includes/templates/responsive_classic/common/tpl_main_page.php
@@ -166,4 +166,15 @@ if  ($detect->isMobile() && !$detect->isTablet() || $_SESSION['layoutType'] == '
 <?php /* add any end-of-page code via an observer class */
 $zco_notifier->notify('NOTIFY_FOOTER_END', $current_page);
 ?>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-87299509-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 </body>
