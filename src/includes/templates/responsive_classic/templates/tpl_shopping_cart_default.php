c286fe66e20ed7046dbd828a8d3bc564b7eac754
chenliujin <liujin.chen@qq.com>
2016-11-18 18:09:20 +0800

    remove

--- src/includes/templates/responsive_classic/templates/tpl_shopping_cart_default.php
+++ src/includes/templates/responsive_classic/templates/tpl_shopping_cart_default.php
@@ -74,10 +74,6 @@ if ($flagHasCartContents) { ?>
 		<td class="price size-medium text-bold  cartUnitDisplay">
 			<div style="margin: 14px 0">
 			<?php 
-			if ( $detect->isMobile() && !$detect->isTablet() || $_SESSION['layoutType'] == 'mobile' or $detect->isTablet() || $_SESSION['layoutType'] == 'tablet' ) {
-				echo '<b class="hide">' . TABLE_HEADING_PRICE . '&#58;&nbsp;&nbsp;</b>'; 
-			} 
-
 			echo $product['productsPriceEach']; 
 			?>
 			</div>
