71bd7633db1e2455047effed4071b4dfaf371fa1
chenliujin <liujin.chen@qq.com>
2016-11-18 21:26:05 +0800

    update

--- src/includes/modules/order_total/ot_subtotal.php
+++ src/includes/modules/order_total/ot_subtotal.php
@@ -1,8 +1,10 @@
 <?php
-class ot_subtotal {
+class ot_subtotal 
+{
 	var $title, $output;
 
-	function __construct() {
+	function __construct() 
+	{
 		$this->code = 'ot_subtotal';
 		$this->title = MODULE_ORDER_TOTAL_SUBTOTAL_TITLE;
 		$this->description = MODULE_ORDER_TOTAL_SUBTOTAL_DESCRIPTION;
@@ -23,10 +25,11 @@ class ot_subtotal {
 			'title' => $this->title . ':',
 			'text' 	=> $currencies->format($order->info['subtotal'], true, $order->info['currency'], $order->info['currency_value']),
 			'value' => $order->info['subtotal']
-			];
+		];
 	}
 
-	function check() {
+	function check() 
+	{
 		global $db;
 		if (!isset($this->_check)) {
 			$check_query = $db->Execute("select configuration_value from " . TABLE_CONFIGURATION . " where configuration_key = 'MODULE_ORDER_TOTAL_SUBTOTAL_STATUS'");
@@ -36,17 +39,20 @@ class ot_subtotal {
 		return $this->_check;
 	}
 
-	function keys() {
+	function keys() 
+	{
 		return array('MODULE_ORDER_TOTAL_SUBTOTAL_STATUS', 'MODULE_ORDER_TOTAL_SUBTOTAL_SORT_ORDER');
 	}
 
-	function install() {
+	function install() 
+	{
 		global $db;
 		$db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('This module is installed', 'MODULE_ORDER_TOTAL_SUBTOTAL_STATUS', 'true', '', '6', '1','zen_cfg_select_option(array(\'true\'), ', now())");
 		$db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Sort Order', 'MODULE_ORDER_TOTAL_SUBTOTAL_SORT_ORDER', '100', 'Sort order of display.', '6', '2', now())");
 	}
 
-	function remove() {
+	function remove() 
+	{
 		global $db;
 		$db->Execute("delete from " . TABLE_CONFIGURATION . " where configuration_key in ('" . implode("', '", $this->keys()) . "')");
 	}
