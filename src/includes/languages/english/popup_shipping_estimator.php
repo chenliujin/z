8b0c54aa38b5ea02f00ac187adf9dbe35c4f11c0
chenliujin <liujin.chen@qq.com>
2016-11-21 16:46:05 +0800

    remove

--- src/includes/languages/english/popup_shipping_estimator.php
+++
@@ -1,5 +0,0 @@
-<?php
-define('HEADING_SEARCH_HELP', 'Shipping Estimator:');
-define('TEXT_TOTAL_ITEMS', 'Total Items: ');
-define('TEXT_TOTAL_WEIGHT', '&nbsp;&nbsp;Weight: ');
-define('TEXT_TOTAL_AMOUNT', '&nbsp;&nbsp;Amount: ');
