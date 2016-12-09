63c2b174edacd7237484bfd7c343c98f82ed449f
chenliujin <liujin.chen@qq.com>
2016-12-09 11:19:10 +0800

    format

--- src/admin/attributes_controller.php
+++ src/admin/attributes_controller.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * @package admin
- * @copyright Copyright 2003-2016 Zen Cart Development Team
- * @copyright Portions Copyright 2003 osCommerce
- * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: Author: DrByte  Thu Mar 3 12:16:32 2016 -0500 Modified in v1.5.5 $
- */
   require('includes/application_top.php');
 
   // troubleshooting/debug of option name/value IDs:
