957a554cb75afa0aee38fe08643ee3497d07998c
chenliujin <liujin.chen@qq.com>
2016-12-13 15:47:58 +0800

    update

--- src/admin/includes/modules/product/collect_info.php
+++ src/admin/includes/modules/product/collect_info.php
@@ -477,9 +477,7 @@ updateGross();
 		<div>
 		<?php for ($i=0, $n=sizeof($languages); $i<$n; $i++) { ?>
 			<label><?php echo zen_image(DIR_WS_CATALOG_LANGUAGES . $languages[$i]['directory'] . '/images/' . $languages[$i]['image'], $languages[$i]['name']) . '&nbsp' . $languages[$i]['name']; ?></label>
-			<?php 
-	  		echo zen_draw_input_field('products_name[' . $languages[$i]['id'] . ']', htmlspecialchars(isset($products_name[$languages[$i]['id']]) ? stripslashes($products_name[$languages[$i]['id']]) : zen_get_products_name($pInfo->products_id, $languages[$i]['id']), ENT_COMPAT, CHARSET, TRUE), zen_set_field_length(TABLE_PRODUCTS_DESCRIPTION, 'products_name')); 
-			?>
+			<input name="<?php echo 'products_name[' . $languages[$i]['id'] . ']'; ?>" value="<?php echo htmlspecialchars(isset($products_name[$languages[$i]['id']]) ? stripslashes($products_name[$languages[$i]['id']]) : zen_get_products_name($pInfo->products_id, $languages[$i]['id']), ENT_COMPAT, CHARSET, TRUE); ?>" style="width: 75%" />
 		<?php } ?>
 		</div>
 	</fieldset>
